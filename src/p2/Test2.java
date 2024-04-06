3f780a38e1cdda2adc94c11e82a9e1985e550de9
Prathamesh Tati <tati.pratham@gmail.com>
2024-04-06 11:29:20 +0530

    Added line 6 and 7

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -3,6 +3,8 @@ package p2;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("In test 2");
+		System.out.println("Added line 6 in Branch B");
+		System.out.println("Added line 7 in Branch B");
 	}
 
 
