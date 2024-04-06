b3b8397081d192ea471622fdce0e7ee8589b6384
Prathamesh Tati <tati.pratham@gmail.com>
2024-04-06 11:14:34 +0530

    conflicts resolved

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("In test 1");
 		System.out.println("Line no 6 changed in test 1");
+		System.out.println("Added Line 7 in B");
+
 	}
 }
